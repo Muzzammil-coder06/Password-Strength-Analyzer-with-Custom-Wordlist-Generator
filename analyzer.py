c86e291df72c41300ecbc81ca586e8700332bc2d
Muzzammil-coder06 <muzzammil25edu@gmail.com>
2025-06-23 14:37:47 +0530

    Add files via upload

---
+++ analyzer.py
@@ -0,0 +1,10 @@
+from zxcvbn import zxcvbn
+
+def analyze_password(password):
+    result = zxcvbn(password)
+    return {
+        "password": password,
+        "score": result["score"],
+        "crack_time": result["crack_times_display"]["offline_fast_hashing_1e10_per_second"],
+        "feedback": result["feedback"]
+    }
